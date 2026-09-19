46906059c09fa36dadbb0e0fb4f5f8f5a6e19ae2
Chinmay <chinmay2003cp@gmail.com>
2026-09-19 12:58:59 +0530

    Reduced cleanup time to 2 days

--- server.js
+++ server.js
@@ -163,7 +163,7 @@ app.get("/stats", (req, res) => {
 });
 
 function cleanupUnknown() {
-  const cutoff = Date.now() - (7 * 24 * 60 * 60 * 1000); // 7 days ago
+  const cutoff = Date.now() - (2 * 24 * 60 * 60 * 1000); // 2 days ago
 
   const knownPathsList = db.prepare(`SELECT service, path FROM known_paths`).all();
 
